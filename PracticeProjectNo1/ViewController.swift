78d535a2a7649925efbbd1f13c04fbff43c37998
Jahongirjon Xoliqov <j.xoliqov@corp.click.uz>
2023-08-19 10:50:35 +0500

    Initial Commit

---
+++ PracticeProjectNo1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeProjectNo1
+//
+//  Created by Dzakhon on 19/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
